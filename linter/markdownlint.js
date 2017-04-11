5e927e757155cad9e61a84cf25ce936ef72d80d0
Francesco Lodolo (:flod) <flodolo@mozilla.com>
2017-04-11 13:33:24 +0200

    Improve portability

--- linter/markdownlint.js
+++ linter/markdownlint.js
@@ -1,18 +1,41 @@
-var markdownlint = require('markdownlint');
-var Glob = require('glob')
+const fs = require('fs');
+const glob = require('glob');
+const markdownlint = require('markdownlint');
+const path = require('path');
+
+// Default paths and configs
+const argv = require('minimist')(process.argv.slice(2));
+
+const default_config = path.join(path.dirname(require.main.filename), 'markdownlint.json');
+var config_file = 'config' in argv
+    ? path.resolve(argv['config'])
+    : default_config;
+if (!fs.existsSync(config_file)) {
+    console.log('Config file doesn\'t exist. Falling back to default.');
+    config_file = default_config;
+}
+console.log('Using config file: ' + config_file);
+
+const search_path = 'path' in argv
+    ? path.resolve(argv['path'])
+    : process.cwd();
+console.log('Searching path: ' + search_path)
 
 // Get all .md files
-var filelist = Glob.sync(
-    '**/*.md',
+const filelist = glob.sync(
+    path.join(search_path, '**/*.md'),
     {
-        'ignore': 'node_modules/**'
+        'ignore': [
+            '**/node_modules/**',
+        ]
     });
 
-var options = {
+// Run linter
+const options = {
     'files': filelist,
-    'config': require('./markdownlint.json')
+    'config': require(config_file)
 };
-var results = markdownlint.sync(options);
+const results = markdownlint.sync(options);
 console.log(results.toString(true));
 
 if (results.toString(true)) {
